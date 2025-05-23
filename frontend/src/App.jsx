2396c44fece100a634729708bbac4ae6b72acefa
kavindupbsr <bkpabasara@gmail.com>
2025-05-23 12:54:40 +0530

    feat: add routing with react-router-dom and create basic pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,20 @@
-import {Button} from '@chakra-ui/react'
+import {Box, Button} from '@chakra-ui/react'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import CreatePage from './pages/CreatePage';
+
 
 function App() {
 
   return (
-    <>
-      
-      <Button>Click me</Button>
-      <Button>Click me</Button>
-    </>
-  )
+  <Box minH={"100vh"}>
+    {/*navbar*/}
+    <Routes>
+      <Route path="/" element={<HomePage/>} />
+      <Route path="/create" element={< CreatePage/>} />
+    </Routes>
+  </Box>
+  );
 }
 
 export default App
